8b70b4e72bc9e5f202b0c1b91eaa35048c2dbfc7
xiongxikai <xx64@njit.edu>
2020-05-27 12:59:30 -0400

    add delete method for teacher controller and add crossOrigal

--- guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
@@ -4,14 +4,13 @@ package com.guli.edu.controller.admin;
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/admin/edu/teacher")
+@CrossOrigin //跨域  protocal hostname port --> cors issue
 public class TeacherAdminController {
 
     @Autowired
@@ -20,5 +19,10 @@ public class TeacherAdminController {
     @GetMapping
     public List<Teacher> list() {
         return teacherService.list(null);
+    }
+
+    @DeleteMapping("{id}")
+    public boolean removeById(@PathVariable String id) {
+        return teacherService.removeById(id);
     }
 }
