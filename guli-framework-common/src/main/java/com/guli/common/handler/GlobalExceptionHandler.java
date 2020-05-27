830395226744f3bf44cad798eca7d7b73824a489
xiongxikai <xx64@njit.edu>
2020-05-27 18:47:11 -0400

    handle expected exceptions defined in ResultCodeEnum

--- guli-framework-common/src/main/java/com/guli/common/handler/GlobalExceptionHandler.java
+++ guli-framework-common/src/main/java/com/guli/common/handler/GlobalExceptionHandler.java
@@ -7,17 +7,42 @@ package com.guli.common.handler;
  * 5
  */
 
+import com.guli.common.constants.ResultCodeEnum;
 import com.guli.common.vo.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public R error(Exception e) {
-        e.printStackTrace();
+        log.error(e.getMessage());
+        //e.printStackTrace();
         return R.error();
+    }
+
+
+    @ExceptionHandler(BadSqlGrammarException.class)
+    @ResponseBody
+    public R error(BadSqlGrammarException e) {
+        log.error(e.getMessage());
+//        e.printStackTrace();
+//        return R.error().code(200003).message("sql syctax error");
+        return R.setResult(ResultCodeEnum.BAD_SQL_GRAMMAR);
+    }
+
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseBody
+    public R error(DataIntegrityViolationException e) {
+        log.error(e.getMessage());
+//        e.printStackTrace();
+        return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
     }
 }
