fe843a17710e182ed5af1460acb57cb00a3a0f5a
xiongxikai <xx64@njit.edu>
2020-05-27 19:18:05 -0400

    config log path and unite console and sl4j log

--- guli-framework-common/src/main/java/com/guli/common/handler/GlobalExceptionHandler.java
+++ guli-framework-common/src/main/java/com/guli/common/handler/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ package com.guli.common.handler;
 
 import com.guli.common.constants.ResultCodeEnum;
 import com.guli.common.exception.GuliException;
+import com.guli.common.util.ExceptionUtil;
 import com.guli.common.vo.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -23,8 +24,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public R error(Exception e) {
-        log.error(e.getMessage());
+        //log.error(e.getMessage());
         //e.printStackTrace();
+        log.error(ExceptionUtil.getMessage(e));
         return R.error();
     }
 
@@ -32,9 +34,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadSqlGrammarException.class)
     @ResponseBody
     public R error(BadSqlGrammarException e) {
-        log.error(e.getMessage());
+        //log.error(e.getMessage());
 //        e.printStackTrace();
 //        return R.error().code(200003).message("sql syctax error");
+        log.error(ExceptionUtil.getMessage(e));
         return R.setResult(ResultCodeEnum.BAD_SQL_GRAMMAR);
     }
 
@@ -42,8 +45,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseBody
     public R error(DataIntegrityViolationException e) {
-        log.error(e.getMessage());
+        //log.error(e.getMessage());
 //        e.printStackTrace();
+        log.error(ExceptionUtil.getMessage(e));
         return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
     }
 
@@ -51,7 +55,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(GuliException.class)
     @ResponseBody
     public R error(GuliException e){
-        e.printStackTrace();
+        //e.printStackTrace();
+        log.error(ExceptionUtil.getMessage(e));
         return R.error().message(e.getMessage()).code(e.getCode());
     }
 }
